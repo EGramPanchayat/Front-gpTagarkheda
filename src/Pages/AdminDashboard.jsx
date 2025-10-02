1fb266f11b1be40beeecaa25766580c2c61707a9
EGramPanchayat <anwit3171@gmail.com>
2025-10-02 18:09:16 +0530

    Initial commit

--- src/Pages/AdminDashboard.jsx
+++ src/Pages/AdminDashboard.jsx
@@ -304,7 +304,7 @@ export default function AdminDashboard() {
             />
             <div className="flex flex-col">
               <h1 className="text-lg md:text-xl font-bold tracking-wide whitespace-nowrap">
-                ग्रामपंचायत आंबेवाडी
+                ग्रामपंचायत गोमेवाडी 
               </h1>
               <span className="text-sm md:text-base text-white/80">
                 ता. आटपाडी जि. सांगली
